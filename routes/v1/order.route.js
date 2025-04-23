c220b0094fab5aaeba2f46ee00d5eb5dfea4c372
antor10655gh <creativefreelancerantor@gmail.com>
2025-04-23 17:47:25 +0600

    update the order route

--- routes/v1/order.route.js
+++ routes/v1/order.route.js
@@ -3,19 +3,21 @@ const app = express.Router();
 
 const ordersController = require("../../controllers/orders.controller");
 
+const { auth } = require("../../middleware/auth");
+
 // Place a new order
-app.post("/", ordersController.placeOrder);
+app.post("/", auth, ordersController.placeOrder);
 
 // Get all orders
-app.get("/", ordersController.getOrders);
+app.get("/", auth, ordersController.getOrders);
 
 // Get a single order by ID
-app.get("/:id", ordersController.getOrderById);
+app.get("/:id", auth, ordersController.getOrderById);
 
 // Update order status
-app.put("/:id", ordersController.updateOrderStatus);
+app.put("/:id", auth, ordersController.updateOrderStatus);
 
 // Delete an order
-app.delete("/:id", ordersController.deleteOrder);
+app.delete("/:id", auth, ordersController.deleteOrder);
 
 module.exports = app;
