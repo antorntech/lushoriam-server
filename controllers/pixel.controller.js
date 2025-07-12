ccafc83ac703c4a7047df537a886564008e15369
antorntech <creativefreelancerantor@gmail.com>
2025-07-13 01:21:12 +0600

    update fbPixel and order controller

--- controllers/pixel.controller.js
+++ controllers/pixel.controller.js
@@ -1,27 +1,3 @@
-// // controllers/pixel.controller.js
-
-// const { sendServerSideEvent, hashEmail } = require("../utils/fbPixel");
-
-// exports.trackPageView = async (req, res) => {
-//   try {
-//     await sendServerSideEvent({
-//       eventName: "PageView",
-//       userData: {
-//         client_ip_address: req.ip,
-//         client_user_agent: req.headers["user-agent"],
-//         em: req.body.email ? [hashEmail(req.body.email)] : [],
-//       },
-//       eventData: {},
-//     });
-
-//     res.status(200).json({ message: "✅ PageView Tracked" });
-//   } catch (error) {
-//     console.error("❌ PageView Track Error:", error.message);
-//     res.status(500).json({ message: "Failed to track PageView" });
-//   }
-// };
-
-// controllers/pixel.controller.js
 const { sendServerSideEvent, hashEmail } = require("../utils/fbPixel");
 
 exports.trackPageView = async (req, res) => {
